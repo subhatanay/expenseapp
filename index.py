483c5cebbdd7b8856489cae0e349419e0f481a07
subhajit <itsme.subhajitghosh@gmail.com>
2025-04-18 20:17:51 +0530

    add expense whatsapp bot

--- index.py
+++ index.py
@@ -1,2 +1,10 @@
-def handler(request):
-    return ("Hello from Vercel Python!", 200, {"Content-Type": "text/plain"})
+from http.server import BaseHTTPRequestHandler
+ 
+class handler(BaseHTTPRequestHandler):
+ 
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header('Content-type','text/plain')
+        self.end_headers()
+        self.wfile.write('Hello, world!'.encode('utf-8'))
+        return
