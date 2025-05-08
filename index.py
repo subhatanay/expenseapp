c15fff86a3c4ae118ecbb59b4433ae7534025236
subhajit <itsme.subhajitghosh@gmail.com>
2025-05-08 09:45:23 +0530

    support read active user email config, add staged transactions, show staged items and tag them

--- index.py
+++ index.py
@@ -396,7 +396,7 @@ def get_user_by_user_id(user_id, cur):
     try:
         cur.execute("SELECT * FROM users WHERE id = %s", (user_id,))
         row = cur.fetchone()
-        return json.loads(row) if row else None
-    except Exception:
+        return dict(row) if row else None
+    except Exception as e:
         logging.error("Error fetching user info", exc_info=True)
         return None
