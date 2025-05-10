c593ac4e57f7c1deb29d3bacb61951607e31b41b
subhajit <itsme.subhajitghosh@gmail.com>
2025-05-10 18:42:58 +0530

    added notification api to send whatsapp

--- index.py
+++ index.py
@@ -21,6 +21,7 @@ TWILIO_ACCOUNT_SID = os.getenv("TWILIO_ACCOUNT_SID")
 TWILIO_AUTH_TOKEN = os.getenv("TWILIO_AUTH_TOKEN")
 TWILIO_WHATSAPP_FROM = "whatsapp:+14155238886"  # Twilio Sandbox number
 
+client = Client(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN)
 
 # DB connection
 def get_conn():
